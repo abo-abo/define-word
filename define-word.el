0914d8dcad5ff76062c062ebc2cece21986fdaad
Oleh Krehel <ohwoeowho@gmail.com>
2015-05-22 13:42:11 +0200

    define-word.el: Add license

--- define-word.el
+++ define-word.el
@@ -7,6 +7,21 @@
 ;; Version: 0.1.0
 ;; Keywords: dictionary, convenience
 
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; This package will send an anonymous request to http://wordnik.com/
