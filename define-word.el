9c9c9334e58e27efb9391b2be74b453f73bd1c2e
Oleh Krehel <ohwoeowho@gmail.com>
2015-05-22 15:07:39 +0200

    define-word.el: Require at least Emacs 24.1

--- define-word.el
+++ define-word.el
@@ -5,6 +5,7 @@
 ;; Author: Oleh Krehel <ohwoeowho@gmail.com>
 ;; URL: https://github.com/abo-abo/define-word
 ;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: dictionary, convenience
 
 ;; This file is not part of GNU Emacs
